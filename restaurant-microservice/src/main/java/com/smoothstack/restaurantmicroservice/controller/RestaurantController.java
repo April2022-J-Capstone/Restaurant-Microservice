6644db57a0879e266292a465cd1ddf020d53ba97
Sinblio-SmoothStack <benls@iastate.edu>
2022-07-08 08:38:32 -0500

    updated properties to enviroment variable db configuration

--- restaurant-microservice/src/main/java/com/smoothstack/restaurantmicroservice/controller/RestaurantController.java
+++ restaurant-microservice/src/main/java/com/smoothstack/restaurantmicroservice/controller/RestaurantController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class RestaurantController {
 
